6fb2723a5e7aa10b5d3ec3f7bc1e61db9c561744
charmoniumQ <sag150430@utdallas.edu>
2016-11-09 01:20:24 -0600

    added initial models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    title = models.CharField(max_length=127)
+    description = models.TextField()
+    solution = models.TextField()
+    answer = models.IntegerField()
+    answer_prog = models.TextField()
+
+class User(models.Model):
+    username = models.CharField(max_length=127)
+    email = models.CharField(max_length=127)
+    firstname = models.CharField(max_length=127)
+    lastname = models.CharField(max_length=127)
+    levels = models.IntegerField()
+    points = models.IntegerField()
+
+class UserProblem(models.Model):
+    user = models.ForeignKey(User)
+    problem = models.ForeignKey(Problem)
+    # comma separated string with previous guesses
+    previous = models.CharField(max_length=127)
+    # number of guesses, negative if correct
+    guesses = models.IntegerField()
